799cea61f7f3ac2184f1e03722b02cc504c3d1c3
Justin Liu <justin.xin.liu@gmail.com>
2019-08-28 16:55:57 -0700

    Working on flying cards.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,114 @@
 import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
+import { useSpring, useSprings, animated, interpolate } from "react-spring";
+import { useDrag } from "react-use-gesture";
 import "./App.css";
 
-const calc = (x, y) => [
-  -(y - window.innerHeight / 2) / 40,
-  (x - window.innerWidth / 2) / 40,
-  1.01
+const cards = [
+  "https://images.unsplash.com/photo-1566863244489-a5e7946f46f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80",
+  "https://images.unsplash.com/photo-1566863244489-a5e7946f46f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80",
+  "https://images.unsplash.com/photo-1566863244489-a5e7946f46f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80",
+  "https://images.unsplash.com/photo-1566863244489-a5e7946f46f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80",
+  "https://images.unsplash.com/photo-1566863244489-a5e7946f46f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80",
+  "https://images.unsplash.com/photo-1566863244489-a5e7946f46f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
 ];
-const trans = (x, y, s) =>
-  `perspective(2400px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+// These two are just helpers, they curate spring data, values that are later being interpolated into css
+const to = i => ({
+  x: 0,
+  y: i * -4,
+  scale: 1,
+  rot: -10 + Math.random() * 20,
+  delay: i * 100
+});
+const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+// This is being used down there in the view, it interpolates rotation and scale into a css transform
+const transform = (r, s) =>
+  `perspective(1500px) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`;
+
+function ZStackCardView() {
+  const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
+  const [props, set] = useSprings(cards.length, i => ({
+    ...to(i),
+    from: from(i)
+  })); // Create a bunch of springs using the helpers above
+  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
+  const bind = useDrag(
+    ({
+      args: [index],
+      down,
+      delta: [xDelta, yDelta],
+      direction: [xDir],
+      velocity
+    }) => {
+      const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
+      const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
+      if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+      set(i => {
+        if (index !== i) return; // We're only interested in changing spring-data for the current spring
+        const isGone = gone.has(index);
+        const x = isGone ? (window.innerWidth / 2) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+        const y = isGone ? window.innerHeight : down ? yDelta : 0;
+        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
+        const scale = down ? 1.1 : 1; // Active cards lift up a bit
+
+        console.log("window.innerWidth", window.innerWidth);
+        console.log("window.innerHeight", window.innerHeight);
+
+        return {
+          x,
+          y,
+          rot,
+          scale,
+          delay: undefined,
+          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 }
+        };
+      });
+      if (!down && gone.size === cards.length)
+        setTimeout(() => gone.clear() || set(i => to(i)), 600);
+    }
+  );
+  // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+  return props.map(({ x, y, rot, scale }, i) => (
+    <animated.div
+      className="outer"
+      key={i}
+      style={{
+        transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`)
+      }}
+    >
+      {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+      <animated.div
+        className="inner"
+        {...bind(i)}
+        style={{
+          transform: interpolate([rot, scale], transform),
+          backgroundImage: `url(${cards[i]})`
+        }}
+      />
+    </animated.div>
+  ));
+}
+
+// const calc = (x, y) => [
+//   -(y - window.innerHeight / 2) / 40,
+//   (x - window.innerWidth / 2) / 40,
+//   1.01
+// ];
+// const transform = (x, y, s) =>
+//   `perspective(2400px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 function App() {
-  const [restShadow, setRestShadow] = useState(false);
+  // const [restShadow, setRestShadow] = useState(false);
 
-  const [props, set] = useSpring(() => ({
-    xys: [0, 0, 1],
-    config: { mass: 2, tension: 400, friction: 36 }
-  }));
+  // const [props, set] = useSpring(() => ({
+  //   xys: [0, 0, 1],
+  //   config: { mass: 2, tension: 400, friction: 36 }
+  // }));
 
   return (
     <div className="app">
       <div className="card-container">
-        <animated.div
+        {/* <animated.div
           className={`card ${restShadow ? "rest-shadow" : ""}`}
           onPointerEnter={() => setRestShadow(false)}
           onPointerMove={({ clientX: x, clientY: y }) =>
@@ -29,8 +116,10 @@ function App() {
           }
           onPointerOut={() => set({ xys: [0, 0, 1] })}
           onTouchEnd={() => setRestShadow(true)}
-          style={{ transform: props.xys.interpolate(trans) }}
-        />
+          style={{ transform: props.xys.interpolate(transform) }}
+        /> */}
+
+        <ZStackCardView />
       </div>
     </div>
   );
