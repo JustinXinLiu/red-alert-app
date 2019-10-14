9aa3a3d2f6cd1eed161989ce8ea65276b4a1f8b0
Justin Liu <justin.xin.liu@gmail.com>
2019-10-14 16:09:26 -0700

    Fixed opacity for top & bottom bars.

--- src/components/ActionBar/ActionBar.js
+++ src/components/ActionBar/ActionBar.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./ActionBar.scss";
 import { InboxMenu, ReminderMenu } from "../../components";
+import { EmailViewMode, useStateValue } from "../../Store";
+import { useTransition, animated } from "react-spring";
 
 function ActionBar(props) {
-  return (
-    <div className="ActionBar">
-      <ReminderMenu />
-      <InboxMenu />
-    </div>
+  const [{ emailViewMode }] = useStateValue();
+
+  const transitions = useTransition(
+    emailViewMode === EmailViewMode.preview,
+    null,
+    {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 }
+    }
+  );
+
+  return transitions.map(
+    ({ item, key, props }) =>
+      item && (
+        <animated.div className="ActionBar" key={key} style={props}>
+          <ReminderMenu />
+          <InboxMenu />
+        </animated.div>
+      )
   );
 }
 
