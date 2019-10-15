4751b3caf980bca94a3e6fb3678143090a122b36
Justin Liu <justin.xin.liu@gmail.com>
2019-10-14 17:05:28 -0700

    Added offset transition.

--- src/components/ActionBar/ActionBar.js
+++ src/components/ActionBar/ActionBar.js
@@ -11,9 +11,9 @@ function ActionBar(props) {
     emailViewMode === EmailViewMode.preview,
     null,
     {
-      from: { opacity: 0 },
-      enter: { opacity: 1 },
-      leave: { opacity: 0 }
+      from: { opacity: 0, transform: "translate3d(0,25%,0)" },
+      enter: { opacity: 1, transform: "translate3d(0,0,0)" },
+      leave: { opacity: 0, transform: "translate3d(0,25%,0)" }
     }
   );
 
--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -17,9 +17,9 @@ function AppBar(props) {
     emailViewMode === EmailViewMode.preview,
     null,
     {
-      from: { opacity: 0 },
-      enter: { opacity: 1 },
-      leave: { opacity: 0 }
+      from: { opacity: 0, transform: "translate3d(0,-25%,0)" },
+      enter: { opacity: 1, transform: "translate3d(0,0,0)" },
+      leave: { opacity: 0, transform: "translate3d(0,-25%,0)" }
     }
   );
 
